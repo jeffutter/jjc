8b64ac1d84120378fea97f40b331dd691afd56d6
Jeffery Utter <jeff@jeffutter.com>
2019-10-17 08:49:25 -0500

    Initial Commit

---
+++ cmd/bulk-transition.go
@@ -0,0 +1,96 @@
+/*
+Copyright © 2019 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	jira "gopkg.in/andygrunwald/go-jira.v1"
+	"os"
+	"strings"
+)
+
+var SourceStatus string
+var DestStatus string
+
+var bulkTransitionCmd = &cobra.Command{
+	Use:   "bulk-transition",
+	Short: "Change the status of all jira tickets for a given user from source to dest",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		jiraUsername := strings.TrimSpace(os.Getenv("JIRA_USERNAME"))
+		jiraToken := strings.TrimSpace(os.Getenv("JIRA_TOKEN"))
+		jiraHost := strings.TrimSpace(os.Getenv("JIRA_HOST"))
+
+		fmt.Printf("\nTransitioning all tickets for %s in %s status to %s", jiraUsername, SourceStatus, DestStatus)
+
+		tp := jira.BasicAuthTransport{
+			Username: jiraUsername,
+			Password: jiraToken,
+		}
+
+		client, err := jira.NewClient(tp.Client(), jiraHost)
+		if err != nil {
+			panic(err)
+		}
+
+		issues, _, err := client.Issue.Search(fmt.Sprintf("assignee=currentUser() and status=\"%s\"", SourceStatus), nil)
+		if err != nil {
+			panic(err)
+		}
+
+		if len(issues) == 0 {
+			fmt.Printf("\nThere are no issues in %s Status\n", SourceStatus)
+		}
+
+		for ii := range issues {
+			issueID := issues[ii].ID
+
+			transitions, _, err := client.Issue.GetTransitions(issueID)
+			if err != nil {
+				panic(err)
+			}
+
+			for ti := range transitions {
+				if transitions[ti].Name == DestStatus {
+					fmt.Printf("\nTransitioning: %s\n", issueID)
+
+					_, err := client.Issue.DoTransition(issueID, transitions[ti].ID)
+					if err != nil {
+						panic(err)
+					}
+
+					fmt.Printf("\n%s Transitioned to %s\n", issues[ii].Fields.Summary, DestStatus)
+				}
+				break
+			}
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(bulkTransitionCmd)
+
+	bulkTransitionCmd.Flags().StringVarP(&SourceStatus, "source", "s", "", "Source status to transition from")
+	bulkTransitionCmd.Flags().StringVarP(&DestStatus, "dest", "d", "", "Dest status to transition to")
+	bulkTransitionCmd.MarkFlagRequired("source")
+	bulkTransitionCmd.MarkFlagRequired("dest")
+}
